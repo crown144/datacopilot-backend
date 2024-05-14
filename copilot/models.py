868866246dfd4842fb37b0a208cbd1532cbafa2a
蒋文强 <12723741+jiang-wenqiang123@user.noreply.gitee.com>
2024-05-14 17:02:38 +0800

    数据库连接

--- copilot/models.py
+++ copilot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    userid = models.BigAutoField(db_column='UserID', primary_key=True)  # Field name made lowercase.
+    username = models.CharField(db_column='Username', max_length=255)  # Field name made lowercase.
+    password = models.CharField(db_column='Password', max_length=255)  # Field name made lowercase.
+    email = models.CharField(db_column='Email', max_length=255)  # Field name made lowercase.
+    role = models.CharField(db_column='Role', max_length=6)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'users'
--- copilot/views.py
+++ copilot/views.py
@@ -5,13 +5,31 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from support.auth import Myauth
+from rest_framework import serializers
+from copilot.models import Users
 # Create your views here.
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = "__all__"
+
 
 class loginview(APIView):
-    authentication_classes = [Myauth,]
+    # authentication_classes = [Myauth,]
     def get(self, request):
-        #��ȡ�û���Ϣ,�����û���Ϣ
-        user = request.user
-        return Response("hello,"+user)
+        # 创建一个新的Users实例
+        new_user = Users(username='root', password='root123', email='root@example.com', role='管理员')
+        # 保存到数据库
+        new_user.save()
+        user = Users.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+
+class UserView(APIView):
+    def get(self, request):
+        # 获取用户信息,返回用户信息
+        user = Users.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
