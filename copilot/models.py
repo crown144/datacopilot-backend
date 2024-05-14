fb1a7951deae57c8ea7c562fd553e39fb1497efc
crown144 <wangyuxiang826@126.com>
2024-05-14 19:41:02 +0800

    数据库连接

--- copilot/models.py
+++ copilot/models.py
@@ -1,3 +1,21 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+
+class Users(models.Model):
+    userid = models.BigAutoField(db_column='UserID', primary_key=True)  # Field name made lowercase.
+    username = models.CharField(db_column='Username', max_length=255)  # Field name made lowercase.
+    password = models.CharField(db_column='Password', max_length=255)  # Field name made lowercase.
+    email = models.CharField(db_column='Email', max_length=255)  # Field name made lowercase.
+    role = models.CharField(db_column='Role', max_length=6)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'users'
--- copilot/views.py
+++ copilot/views.py
@@ -4,14 +4,28 @@ from rest_framework.decorators import api_view
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
+from copilot.models import Users
 from support.auth import Myauth
+from rest_framework import serializers
 # Create your views here.
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = "__all__"
 
 class loginview(APIView):
-    authentication_classes = [Myauth,]
+    #authentication_classes = [Myauth,]
     def get(self, request):
-        #获取用户信息,返回用户信息
-        user = request.user
-        return Response("hello,"+user)
+        user = Users.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+
+        
+
+class UserView(APIView):
+    def get(self, request):
+        #鑾峰彇鐢ㄦ埛淇℃伅,杩斿洖鐢ㄦ埛淇℃伅
+        user = Users.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
